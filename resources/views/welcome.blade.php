5e02eba8d8bb54650a19bca9ae0fa0f336f6069e
raiku <raiku6019@gmail.com>
2021-06-27 01:01:03 +0900

    modify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <titlen><a href="{{ route('two_factor_auth') }}">Laravel</a></titlen>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
