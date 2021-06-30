edbe24eb5c63ab21e8bc3750dabf98fb5beccc66
raiku <raiku6019@gmail.com>
2021-06-30 17:53:03 +0900

    modify

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -66,7 +66,11 @@
     document.getElementById('logout-form').submit();
     });
 </script>
-<script src="{{ asset('/js/main.js') }}"></script>
+@if(app('env') == 'heroku')
+    <script src="{{ secure_asset('/js/main.js') }}"></script>
+@else
+    <script src="{{ asset('/js/main.js') }}"></script>
+@endif
 @endif
 @yield('scripts')
 </body>
