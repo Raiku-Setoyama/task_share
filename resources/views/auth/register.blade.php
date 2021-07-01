d674307d51761e85a52a5e8e26b5144f0eacf561
raiku <raiku6019@gmail.com>
2021-07-01 12:02:12 +0900

    modify

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,7 +22,7 @@
         </span>
         </a>                
         <div class="nav-btn-group">
-            <a class=" btn nav-btn" href="{{ route('two_factor_auth') }}">ログイン</a>
+            <a class=" btn nav-btn" href="{{ route('login') }}">ログイン</a>
             <a class=" btn nav-btn" href="{{ route('register') }}">会員登録</a>
         </div>
     </nav>
